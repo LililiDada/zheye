0c844da465253ddfc365bedbe173722921dd5bda
leedashan <dashanli@xiaoe-tech.com>
2022-12-28 00:51:46 +0800

    'feat:5-3ValidateInput第二部分—抽象验证规则'

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
     <form action="">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <ValiadateInput :rules="emailRules"></ValiadateInput>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
         <input type="email" class="form-control" id="exampleInputEmail1" v-model="emailRef.val" @blur="validateEmail">
         <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
       </div>
@@ -21,6 +25,7 @@
 import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import ValiadateInput, { RulesProp } from './components/ValiadateInput.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
 const currentUser: UserProps = {
@@ -52,9 +57,15 @@ export default defineComponent({
   name: 'App',
   components: {
     // ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValiadateInput
   },
   setup () {
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+
     const emailRef = reactive({
       val: '',
       error: false,
@@ -74,7 +85,8 @@ export default defineComponent({
       list: testData,
       currentUser,
       emailRef,
-      validateEmail
+      validateEmail,
+      emailRules
     }
   }
 })
