9d1afeadbbaff3afae4446962e9680afe44b26c9
leedashan <dashanli@xiaoe-tech.com>
2022-12-28 11:46:22 +0800

    'feat:5-7ValidateForm编码第一部分-使用插槽slot'

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,19 @@
   <div class="container">
     <GlobalHeader :user="currentUser"></GlobalHeader>
     <!-- <ColumnList :list="list"></ColumnList> -->
-    <form action="">
+    <ValidateForm @form-submit="onFormSubmit">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
         <ValiadateInput :rules="emailRules" v-model="emailVal" placeholder="请输入邮箱地址" type="text"></ValiadateInput>
       </div>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" v-model="emailRef.val" @blur="validateEmail">
-        <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
-      </div>
-      <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <ValiadateInput :rules="passwordRules" v-model="passwordVal" placeholder="请输入密码" type="password"></ValiadateInput>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+      <template #submit>
+        <span class="btn btn-danger">提交</span>
+      </template>
+    </ValidateForm>
   </div>
 </template>
 
@@ -26,6 +23,7 @@ import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import ValiadateInput, { RulesProp } from './components/ValiadateInput.vue'
+import ValidateForm from './components/ValidateForm.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
 const currentUser: UserProps = {
@@ -58,38 +56,35 @@ export default defineComponent({
   components: {
     // ColumnList,
     GlobalHeader,
-    ValiadateInput
+    ValiadateInput,
+    ValidateForm
   },
   setup () {
-    const emailVal = ref('dashan')
+    const emailVal = ref('')
 
     const emailRules: RulesProp = [
       { type: 'required', message: '电子邮箱地址不能为空' },
       { type: 'email', message: '请输入正确的电子邮箱格式' }
     ]
 
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
+    const passwordVal = ref('')
 
-    const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be vaild email'
-      }
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' }
+    ]
+
+    const onFormSubmit = (result: boolean) => {
+      console.log('1222', result)
     }
+
     return {
       list: testData,
       currentUser,
-      emailRef,
-      validateEmail,
       emailRules,
-      emailVal
+      emailVal,
+      passwordVal,
+      passwordRules,
+      onFormSubmit
     }
   }
 })
---
+++ src/components/ValidateForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="validate-form-container">
+    <slot></slot>
+    <div class="submit-area" @click.prevent="submitForm  ">
+      <slot name="submit">
+        <button type="submit" class="btn btn-primary">提交</button>
+      </slot>
+    </div>
+  </form>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, PropType } from 'vue'
+export default defineComponent({
+  emits: ['form-submit'],
+  setup (props, context) {
+    const submitForm = () => {
+      context.emit('form-submit', true)
+    }
+    return {
+      submitForm
+    }
+  }
+})
+</script>
